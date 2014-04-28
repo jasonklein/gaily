c92f166ea9b50e1f71c1de47c8b583e765614082
jasonklein <jasonklein@gmail.com>
2014-04-28 20:40:52 +0100

    Remove person model.

--- app/models/person.rb
+++
@@ -1,8 +0,0 @@
-class Person < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :role, :username
-
-  has_many :enrollments
-  has_many :assignments
-  has_many :courses, through: :enrollments
-  has_many :courses, through: :assignments
-end
