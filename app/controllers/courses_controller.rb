3884759818b30c0898077fa7b1a357d0c2c57045
jasonklein <jasonklein@gmail.com>
2014-04-29 22:47:59 +0100

    Abortive attempt at solving the pre-validation issue of invalid dates.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,9 +13,8 @@ class CoursesController < ApplicationController
     @course = Course.new(params[:course])
 
     dates_cannot_overlap_for_same_classroom
-    raise
     
-    if @result == false
+    if @result && @result == false
       flash[:notice] = "Dates cannot overlap."
       render action: "new"
     else
@@ -42,35 +41,43 @@ class CoursesController < ApplicationController
   def dates_cannot_overlap_for_same_classroom
 
     booking = params[:course][:booking_attributes]
-    new_start_date = Date.new booking["start_date(1i)"].to_i, booking["start_date(2i)"].to_i, booking["start_date(3i)"].to_i
-    new_end_date = Date.new booking["end_date(1i)"].to_i, booking["end_date(2i)"].to_i, booking["end_date(3i)"].to_i
+    
+    if Date.new(booking["end_date(1i)"].to_i, booking["end_date(2i)"].to_i, booking["end_date(3i)"].to_i) && Date.new(booking["start_date(1i)"].to_i, booking["start_date(2i)"].to_i, booking["start_date(3i)"].to_i)
 
-    @result = true
-    i = 0
+      new_start_date ||= Date.new booking["start_date(1i)"].to_i, booking["start_date(2i)"].to_i, booking["start_date(3i)"].to_i
 
-    Booking.where(classroom_id: booking["classroom_id"].to_i).each do |booking|
-      if new_start_date == booking.start_date
-        @result = false
-        i+= 1
-        
-        break
-        
-      elsif (new_start_date > booking.start_date) && (new_start_date <= booking.end_date)
-        @result = false
-        i+= 1
-        
-        break
-        
-      elsif (new_start_date < booking.start_date) && (new_end_date > booking.start_date)
-        @result = false
-        i+= 1
-        raise
-        break
+      new_end_date ||= Date.new booking["end_date(1i)"].to_i, booking["end_date(2i)"].to_i, booking["end_date(3i)"].to_i
+
+      @result = true
+      i = 0
+
+      Booking.where(classroom_id: booking["classroom_id"].to_i).each do |booking|
+        if new_start_date == booking.start_date
+          @result = false
+          i+= 1
+          
+          break
+          
+        elsif (new_start_date > booking.start_date) && (new_start_date <= booking.end_date)
+          @result = false
+          i+= 1
+          
+          break
+          
+        elsif (new_start_date < booking.start_date) && (new_end_date > booking.start_date)
+          @result = false
+          i+= 1
+          raise
+          break
+        end
       end
-    end
 
-    @result
-    raise
+      @result
+      raise
+    else
+      flash[:notice] = "Booking dates are invalid."
+      render action: "new"
+    end
   end
 
 end
