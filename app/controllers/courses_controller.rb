9b976da38399fd7ba8adc5db7cacb03d05c5a1a6
jasonklein <jasonklein@gmail.com>
2014-04-30 13:10:43 +0100

    Make course @course in create method in order to display error messages.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,9 +10,8 @@ class CoursesController < ApplicationController
 
   def create
 
-    course = Course.new(params[:course])
+    @course = Course.new(params[:course])
 
-    #dates_cannot_overlap_for_same_classroom
     validate_dates
 
     if @invalid_date_error
@@ -20,12 +19,13 @@ class CoursesController < ApplicationController
       render action: "new"
     elsif overlapped_bookings?(@new_start_date, @new_end_date)
       overlapped_courses = @overlapped_bookings.map { |booking| booking.course.name }
-      flash[:notice] = "#{course.name} overlaps with #{overlapped_courses.to_sentence}."
+      flash[:notice] = "Course overlaps with #{overlapped_courses.to_sentence}. Please choose different dates."
       render action: "new"
     else
-      if course.save
-        redirect_to course, notice: "#{course.name} has been added."
+      if @course.save
+        redirect_to course, notice: "#{@course.name} has been added."
       else
+        #raise
         render action: "new"
       end
     end
@@ -55,9 +55,7 @@ class CoursesController < ApplicationController
 
   def validate_dates
     booking = params[:course][:booking_attributes]
-    # new_start_date = booking["start_date(1i)"].to_i, booking["start_date(2i)"].to_i, booking["start_date(3i)"].to_i
-    # new_end_date = booking["end_date(1i)"].to_i, booking["end_date(2i)"].to_i, booking["end_date(3i)"].to_i
-
+    
     start_year = booking["start_date(1i)"].to_i
     start_month = booking["start_date(2i)"].to_i
     start_day = booking["start_date(3i)"].to_i
@@ -65,7 +63,6 @@ class CoursesController < ApplicationController
     end_month = booking["end_date(2i)"].to_i
     end_day = booking["end_date(3i)"].to_i
     
-
     if Date.valid_date?(start_year, start_month, start_day) && Date.valid_date?(end_year, end_month, end_day)
       @new_start_date = Date.new(start_year, start_month, start_day)
       @new_end_date = Date.new(end_year, end_month, end_day)
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,17 +3,10 @@ class Booking < ActiveRecord::Base
   belongs_to :classroom
   attr_accessible :end_date, :start_date, :classroom_attributes, :course_id, :classroom_id
 
-
-
-  
-
   validates :course_id, uniqueness: true
-  #validates_date :start_date, on_or_after: :today, on_or_after_message: "must start today or later"
-  #validates_date :end_date, on_or_after: :start_date, on_or_after_message: "must end after start date"
+  validates_date :start_date, on_or_after: :today, on_or_after_message: "must start today or later"
+  validates_date :end_date, on_or_after: :start_date, on_or_after_message: "must end after start date"
 
   accepts_nested_attributes_for :classroom
-
-  # def date_ranges_cannot_overlap
-  #   start_dates = Booking.pluck("start_date")
 
 end
