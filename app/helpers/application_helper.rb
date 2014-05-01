3b1717d580b98995bc8e8a01ed9c855bebf86627
jasonklein <jasonklein@gmail.com>
2014-05-01 10:01:26 +0100

    Add password digest to users.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def active_menu_item(link_path)
+
+    path = Rails.application.routes.recognize_path(link_path)
+
+    controller = path[:controller]
+
+    :active_menu_item if controller == params[:controller]
+
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140430133917) do
+ActiveRecord::Schema.define(:version => 20140501085313) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "course_id"
@@ -60,12 +60,13 @@ ActiveRecord::Schema.define(:version => 20140430133917) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",                        :null => false
-    t.string   "first_name",                      :null => false
-    t.string   "last_name",                       :null => false
-    t.string   "role",       :default => "guest", :null => false
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.string   "username",                             :null => false
+    t.string   "first_name",                           :null => false
+    t.string   "last_name",                            :null => false
+    t.string   "role",            :default => "guest", :null => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["username"], :name => "index_people_on_username", :unique => true
