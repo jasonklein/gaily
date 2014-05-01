25134351702e8cab13bd750fd10a80360fe7ddb7
jasonklein <jasonklein@gmail.com>
2014-05-01 09:52:52 +0100

    Add new sessions view and new/create/destroy actions to sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    user = User.find_by_username(params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      if user.role? :admin
+        redirect_to(users_path, notice: "You have successfully logged in.")
+      else
+        redirect_to(user_path(user), notice: "You have successfully logged in.")
+      end
+    else
+      flash.now.alert = "Invalid username or password."
+      render 'new'
+    end
   end
 
   def destroy
+    session[:current_user_id] = nil
+    redirect_to(root_url, notice: "You have logged out.")
   end
 end
