38bcd1496ba069a6fb78f9e72c104c0dabaae422
jasonklein <jasonklein@gmail.com>
2014-05-01 10:46:36 +0100

    Add user show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,40 @@ load_and_authorize_resource
   end
 
   def create
-  end
+    params[:user][:role] = :member
+    @user = User.new(params[:user])
 
-  def show
+    if @user.save
+      @user = User.find_by_username(@user.username)
+      session[:current_user_id] = @user.id
+      redirect_to(user_path(@user), notice: "You have been successfully added and logged into disastronomia.")
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def show
+  end
+
   def update
+    if @user.update_attributes(params[:user])
+      redirect_to(user_path(@user), notice: "#{@user.username} has been successfully updated.")
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    if session[:current_user_id] == params[:id].to_i
+      User.destroy(params[:id])
+      redirect_to(root_url, notice: "You have been removed.")
+    else
+      User.destroy(params[:id])
+      redirect_to(users_path, notice: "User has been removed.")
+    end
   end
+
 end
